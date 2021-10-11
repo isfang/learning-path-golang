cd85bbb27188ec6a19662a7aff52919b2e8ab4a6
fangdong <fangdong@tuya.com>
2021-10-11 17:09:25 +0800

    mutex copy dead lock

--- concurrency/mutex/mutex_test.go
+++ concurrency/mutex/mutex_test.go
@@ -1,6 +1,11 @@
 package mutex
 
-import "testing"
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
 
 func TestBadCounter(t *testing.T) {
 	badCounter()
@@ -8,4 +13,34 @@ func TestBadCounter(t *testing.T) {
 
 func TestMutex(t *testing.T) {
 	mutex()
+}
+
+func TestTryLock(t *testing.T) {
+	var mu Mutex
+	go func() {
+		mu.Lock()
+		time.Sleep(time.Duration(rand.Intn(2)) * time.Second)
+		mu.Unlock()
+	}()
+	time.Sleep(time.Second)
+	if mu.TryLock() {
+		fmt.Println("got lock")
+		mu.Unlock()
+		return
+	}
+	fmt.Println("can not get lock")
+}
+
+func TestState(t *testing.T) {
+	var mu Mutex
+	for i := 0; i < 1000; i++ {
+		go func() {
+			mu.Lock()
+			time.Sleep(time.Duration(rand.Intn(2)) * time.Second)
+			mu.Unlock()
+		}()
+	}
+
+	time.Sleep(time.Duration(rand.Intn(2)) * time.Second)
+	fmt.Printf("waitings: %d, loaked: %t, woken: %t starving: %t", mu.Count(), mu.IsLocked(), mu.IsWoken(), mu.IsStarving())
 }
